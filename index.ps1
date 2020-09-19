e36d88fb6a5cf1828dd88a8e0ec69826f5140d34
epugh <epugh@opensourceconnections.com>
2020-09-19 09:37:59 -0400

    eye of the newt, toe of the frog.  Please make this curl command work

--- index.ps1
+++ index.ps1
@@ -1,10 +1,10 @@
-# Powershell script for Windows 10 and above to index the TMDB JSON file.
+# PowerShell script for Windows 10 and above to index the TMDB JSON file.
 
 $schema = Get-Content 'schema.json';
-$data = Get-Content 'tmdb_es.json';
+$data = Get-Content -Raw 'tmdb_es.json';
 
-Invoke-WebRequest -Method DELETE -Uri 'http://localhost:9200/tmdb';
+Invoke-WebRequest -Method DELETE -Uri 'http://localhost:9200/tmdb' -UseBasicParsing;
 
 Invoke-WebRequest -Method PUT -Uri 'http://localhost:9200/tmdb' -ContentType 'application/json' -UseBasicParsing -Body $schema;
 
-Invoke-WebRequest -Method POST -Uri 'http://localhost:9200/tmdb' -ContentType 'application/json' -UseBasicParsing -Body $data;
+Invoke-WebRequest -Method POST -Uri 'http://localhost:9200/tmdb/_bulk' -ContentType 'application/json' -UseBasicParsing -Body $data;
